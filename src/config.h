2a239189fa5a3089734e9d592d236a1db0b627d7
Peter Wendling <pawendling@yahoo.com>
2026-03-13 20:26:53 -0400

    Added commit file

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// ---- WiFi ----
+#define WIFI_SSID     "Saddleback-2.4G"
+#define WIFI_PASSWORD "---------"
+
+// ---- MQTT ----
+#define MQTT_SERVER   "192.168.1.100"
+#define MQTT_PORT     1883
+#define MQTT_TOPIC    "sensors/esp32-2"
